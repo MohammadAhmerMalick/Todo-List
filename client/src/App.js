1656b794c529aa51f3d14ab97175b02b4cbbd5b6
MohammadAhmerMalick <mohammadahmermalick@gmail.com>
2021-12-01 22:41:57 +0500

    ui structured

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
+import Header from "./component/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <main>
+        <aside id="navigation-panel">
+
+        </aside>
+        <section id="main-panel">
+
+        </section>
+        <aside id="detail-panel">
+
+        </aside>
+
+      </main>
     </div>
   );
 }
